9b89b222db85469107663690ca351e93229c8432
yogiyulianto <yogi.yulianto@students.amikom.ac.id>
2021-12-11 10:22:19 +0700

    fist commit

--- app.py
+++ app.py
@@ -1,63 +1,48 @@
-from flask import Flask
-import json, nltk
-from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
-from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
-from flaskext.mysql import MySQL
-from nltk.tokenize import word_tokenize 
+from flask import Flask,render_template,request,redirect,url_for
+import pandas as pd
+from sklearn.model_selection import train_test_split
 
-# app = Flask(__name__)
-mysql = MySQL()
 app = Flask(__name__, template_folder='templates')
 
-# MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
-app.config['MYSQL_DATABASE_DB'] = 'telegram_scam_v1_db'
-app.config['MYSQL_DATABASE_HOST'] = '127.0.0.1'
-app.config['MYSQL_DATABASE_PORT'] = 8889
+@app.route("/")
+def hello():
+    # new = modelRF.predict([[1,2022,100,1.52,28,85]])
+    return render_template('index.html')
 
-mysql.init_app(app)
-conn = mysql.connect()
+@app.route('/predict', methods=['GET','POST'])
+def tambah():
+   if request.method == 'POST':
+      angin = request.form['angin']
+      hujan = request.form['hujan']
+      lembab = request.form['lembab']
+      temperatur = request.form['temperatur']
+      bulan = request.form['bulan']
+      tahun = request.form['tahun']
+      df = pd.read_excel("dataset.xlsx")
+      df.dtypes
+      
+      for x in df:
+        if df[x].dtypes == "int64":
+            df[x] = df[x].astype(float)
+            print (df[x].dtypes)
+      df = df.select_dtypes(exclude=['object'])
+      df=df.fillna(df.mean())
+      X = df.drop('hostpot',axis=1)
+      y = df['hostpot']  
+    
+      X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+      from sklearn.ensemble import RandomForestRegressor
+      regressor = RandomForestRegressor(n_estimators = 1000, random_state = 42)
+      modelRF = regressor.fit(X_train, y_train)
+      new = modelRF.predict([[bulan,tahun,hujan,angin,temperatur,lembab]])
+      rst = round(new[0])
+      return render_template('result.html', new=rst)
+   else:
+      return render_template('tambah.html')
 
-# create stemmer
-factory = StemmerFactory()
-stemmer = factory.create_stemmer()
-# open json from telegram
-f = open('telegram.json',)
-jsonData =  json.load(f)
-# stop word
-factory = StopWordRemoverFactory()
-stopword = factory.create_stop_word_remover()
-
-count = 1
-
-for i in jsonData['messages']:
-    if i['type'] == 'message':
-        # init variabel text
-        text = str(i['text'])
-        # text to lowercase
-        lowerCase = text.lower()
-        stemming = stemmer.stem(lowerCase)
-        # stopword removal
-        stopWord = stopword.remove(stemming)
-        count = count + 1
-        textStemming =  str(count) + '. ' + stemming
-        textStopWord =  str(count) + '. ' + stopWord
-        textTokenizing = str(count) + '. ' + str(word_tokenize(stopWord))
-        
-        print(textStemming)
-        print(textStopWord)
-        print(textTokenizing)
+@app.route('/result')
+def result():
+   return render_template('result.html')
             
 if __name__ == '__main__':
     app.run()
-    
-# pseudocode
-# if text != null:
-#     if text tidak berisi kata yang hanya satu huruf , ex: a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z
-#         if text tidak berisi kata singkatan ketika chat, ex: sy, sya, km, kmu
-#         else:
-#         convert kata ke bentuk asli, ex: sya -> saya, km-> kamu
-#    else:
-#    hapus kata yang berisi satu character   
-
